54eca4a6c28ae4effb2b9c8492203c3dd10afe60
Milorad Đuković <donmilorad87@gmail.com>
2024-11-05 23:43:45 +0100

    Assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-const page = () => <h1 className=" font-bold text-violet-700 underline placeholder:text-3xl">Welcome to the next.js app</h1>;
+const page = () => (
+    <>
+        <h1 className="h1-bold background-light850_dark100">Welcome to the next.js app</h1>
+        <h1 className="h1-bold background-light850_dark100 font-space-grotesk">Welcome to the next.js app</h1>
+    </>
+);
 
 export default page;
